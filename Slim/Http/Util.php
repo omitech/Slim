4b8680151fb90ca3e3485f20caee634775d337aa
omitech <omitech@users.noreply.github.com>
2022-11-09 09:13:03 +0100

    remove get_magic_quotes_gpc()

--- Slim/Http/Util.php
+++ Slim/Http/Util.php
@@ -57,11 +57,6 @@ class Util
      */
     public static function stripSlashesIfMagicQuotes($rawData, $overrideStripSlashes = null)
     {
-        $strip = is_null($overrideStripSlashes) ? get_magic_quotes_gpc() : $overrideStripSlashes;
-        if ($strip) {
-            return self::stripSlashes($rawData);
-        }
-
         return $rawData;
     }
 
